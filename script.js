9c90c115a6e5cabeac49bb6c6406a2be00dd63f4
Paras Sharma <parasangiras121@gmail.com>
2022-02-01 17:09:07 +0530

    git config --global user.name "Paras Sharma"

---
+++ script.js
@@ -0,0 +1,27 @@
+const replies = document.querySelectorAll(".reply");
+
+replies.forEach(function(reply){
+    reply.addEventListener("click",function(e){
+       var comment_section = e.currentTarget.parentElement.parentElement.parentElement;
+       var reply_section = comment_section.children[1];
+       reply_section.classList.toggle("show");
+    })
+})
+
+let counter = 0;
+
+const vote_section = document.querySelectorAll(".vote");
+
+vote_section.forEach(function(vote){
+    var upvote = vote.children[0];
+    var downvote = vote.children[2];
+    var count = vote.children[1];
+    upvote.addEventListener("click",function(e){
+        counter++;
+        count.innerHTML = counter;
+    })
+    downvote.addEventListener("click",function(e){
+        counter--;
+        count.innerHTML = counter;
+    })
+})
